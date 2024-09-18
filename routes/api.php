530d3773d225fec343df6939114fb96065af1198
Daniel Supernault <danielsupernault@gmail.com>
2024-09-18 00:37:46 -0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\InstanceController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/instance/check', [InstanceController::class, 'checkInstance']);
