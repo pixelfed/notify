097d63979f27844cee98bb54aa61df59186eeb81
Daniel Supernault <danielsupernault@gmail.com>
2024-09-26 01:21:15 -0600

    Update routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\InstanceController;
+use App\Http\Controllers\RelayV1Controller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/instance/check', [InstanceController::class, 'checkInstance']);
+
+Route::post('/v1/relay/{secret}/{userToken}', [RelayV1Controller::class, 'store']);
