4db80edc7e87603445387e5dbe3e39fb9d1a5f7a
Daniel Supernault <danielsupernault@gmail.com>
2024-10-01 23:53:00 -0600

    Update bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->command('notifications:process')->everyMinute();
+    })
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
