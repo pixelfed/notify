aff37c379efd3e64e49011d90e4fc33402e17262
Daniel Supernault <danielsupernault@gmail.com>
2024-10-02 23:03:59 -0600

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="https://pixelfed-logo.nyc3.cdn.digitaloceanspaces.com/pixelfed-icon-color.svg" class="logo" alt="Pixelfed Logo">
+<img src="https://pixelfed-logo.nyc3.cdn.digitaloceanspaces.com/pixelfed-icon-color.png" class="logo" alt="Pixelfed Logo">
 </a>
 </td>
 </tr>
