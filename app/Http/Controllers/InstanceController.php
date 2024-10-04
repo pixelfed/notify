98ff40c71f85ddc5083696ac205d5df536947d8a
Daniel Supernault <danielsupernault@gmail.com>
2024-10-04 01:45:09 -0600

    Update InstanceController.php

--- app/Http/Controllers/InstanceController.php
+++ app/Http/Controllers/InstanceController.php
@@ -9,10 +9,6 @@ class InstanceController extends Controller
 {
     public function checkInstance(CheckInstanceRequest $request)
     {
-        if (! $request->hasHeader('X-PIXELFED-API')) {
-            return ['active' => false];
-        }
-
         $activeDomains = InstanceService::getActiveDomains();
 
         return [
